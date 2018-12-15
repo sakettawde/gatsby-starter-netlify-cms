cbbbcca8e6e8e73b549fe7ece00c55134b4e0992
Saket Tawde <saket.tawde@gmail.com>
2018-12-15 21:22:47 +0530

    stellar added

---
+++ src/templates/stellar-page.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { graphql } from 'gatsby'
+import Layout from '../components/Layout'
+import Content, { HTMLContent } from '../components/Content'
+
+const StellarPage = () => {
+  
+  return (
+    <Layout>
+      <span>Hello Stellar</span>
+    </Layout>
+  )
+}
+
+
+export default StellarPage
+
