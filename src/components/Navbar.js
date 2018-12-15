44cb3623c627fa10de0392d0c6995d05fc081548
Saket Tawde <saket.tawde@gmail.com>
2018-12-15 21:04:12 +0530

    multiple changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import logo from '../img/logo.png'
 
 const Navbar = class extends React.Component {
 
@@ -35,7 +35,7 @@ const Navbar = class extends React.Component {
     <div className="container">
       <div className="navbar-brand">
         <Link to="/" className="navbar-item" title="Logo">
-          <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+          <img src={logo} alt="Kaldi" style={{ width: '188px' }} />
         </Link>
         {/* Hamburger menu */}
         <div className="navbar-burger burger" data-target="navMenu">
@@ -46,30 +46,24 @@ const Navbar = class extends React.Component {
       </div>
       <div id="navMenu" className="navbar-menu">
       <div className="navbar-start has-text-centered">
-        <Link className="navbar-item" to="/about">
-          About
-        </Link>
-        <Link className="navbar-item" to="/products">
-          Products
-        </Link>
-        <Link className="navbar-item" to="/contact">
-          Contact
-        </Link>
-        <Link className="navbar-item" to="/contact/examples">
-          Form Examples
-        </Link>
+        
       </div>
       <div className="navbar-end has-text-centered">
-        <a
-          className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <span className="icon">
-            <img src={github} alt="Github" />
-          </span>
-        </a>
+      <Link className="navbar-item" to="/about">
+          Home
+        </Link>
+        <Link className="navbar-item" to="/products">
+          Solar for you
+        </Link>
+        <Link className="navbar-item" to="/contact">
+          Services
+        </Link>
+        <Link className="navbar-item" to="/contact/examples">
+          Portfolio
+        </Link>
+        <Link className="navbar-item" to="/contact/examples">
+          Contact Us
+        </Link>
       </div>
       </div>
     </div>
