2df6d8cbc0b1a5885d087204a52751302013af77
Saket Tawde <saket.tawde@gmail.com>
2018-12-16 18:26:12 +0530

    multiple changes

--- src/pages/stellar/index.md
+++ src/pages/stellar/index.md
@@ -1,13 +1,18 @@
 ---
 templateKey: stellar-page
-hero_title: This is hero title
-hero_subtitle: This is hero sub title
+hero_title: Bright Ideas
+hero_subtitle: That will save the world and your pockets
 target_title: Solar for everyone
 target_content:
   - body: We build beautiful solutions
     heading: Make your home
-    image-left: /img/coffee-gear.png
-    image-right: /img/chemex.jpg
+    image_left: /img/coffee-gear.png
+    image_right: /img/chemex.jpg
     subheading: Smart and sustainable
+  - body: We build beautiful solutions as well
+    heading: Make your work place
+    image_left: /img/coffee-gear.png
+    image_right: /img/chemex.jpg
+    subheading: Smart and Sustainable
 ---
 
--- src/templates/stellar-page.js
+++ src/templates/stellar-page.js
@@ -5,11 +5,19 @@ import Layout from '../components/Layout'
 import Content, { HTMLContent } from '../components/Content'
 
 const StellarPage = ({data}) => {  
-  console.log(data)
+  console.log(data.markdownRemark)
+  const {hero_title, hero_subtitle, target_title} = data.markdownRemark.frontmatter
 
   return (
     <Layout>
-      <span>Hello Stellar</span>
+      <div style={{height:"90vh", display:"flex", flexDirection:"column", alignItems:"flex-start", justifyContent:"center", backgroundColor:"#eee", padding:40}}>
+      <span style={{fontSize:48}}>{hero_title}</span>
+      <span  style={{fontSize:24}}>{hero_subtitle}</span>
+      </div>
+      <div style={{height:"90vh", display:"flex", flexDirection:"column", alignItems:"center", justifyContent:"center", backgroundColor:"#eee", padding:40}}>
+      <span style={{fontSize:48}}>{target_title}</span>
+      <span>{hero_subtitle}</span>
+      </div>
     </Layout>
   )
 }
@@ -20,6 +28,30 @@ export const stellarPageQuery = graphql`
   query StellarPage($id: String!) {
     markdownRemark(id: { eq: $id }) {
       html
+      frontmatter{
+        hero_title
+        hero_subtitle
+        target_title
+        target_content{
+          body
+          heading
+          subheading
+          image-left{
+            childImageSharp {
+                fluid(maxWidth: 240, quality: 100) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+          }
+          image-right{
+            childImageSharp {
+                fluid(maxWidth: 240, quality: 100) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+          }
+        }
+      }
     }
   }
 `
